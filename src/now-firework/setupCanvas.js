ae75aaa4223b3132b6d2024908c795701f7abee7
MarcMouries <marc@mouries.net>
2021-12-30 11:58:32 -0500

    set up high def canvas for retina displays

--- src/now-firework/setupCanvas.js
+++ src/now-firework/setupCanvas.js
@@ -2,10 +2,34 @@
  * Initialize the canvas
  */
 export default function setupCanvas({ canvas }) {
-	console.log("IN setupCanvas")
-	console.log("canvas")
-	console.log(canvas);
+  // Get the device pixel ratio, falling back to 1.
+  var dpr = window.devicePixelRatio || 1;
+  // Get the size of the canvas in CSS pixels.
+  var canvasRect = canvas.getBoundingClientRect();
 
-    console.log("canvas width : " + canvas.width);
-	console.log("canvas height : " + canvas.height);
+  // Give the canvas pixel dimensions of their CSS
+  // size * the device pixel ratio.
+  canvas.width = canvasRect.width * dpr;
+  canvas.height = canvasRect.height * dpr;
+
+  const ctx = canvas.getContext('2d')
+
+  // Scale all drawing operations by the dpr, so you 
+  // don't have to worry about the difference.
+  ctx.scale(dpr, dpr);
+
+  let center = { x: canvas.width / 2, y: canvas.height / 2 };
+
+
+  const draw = (frameCount) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    ctx.fillStyle = '#000000'
+    ctx.beginPath()
+    ctx.arc(center.x, center.y, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI)
+    ctx.fill()
+  }
+
+  return {
+    draw
+  };
 }
