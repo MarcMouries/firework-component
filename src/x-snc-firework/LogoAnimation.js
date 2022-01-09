580580abb1a698dc3334b853dacd02aca7177b43
MarcMouries <marc@mouries.net>
2022-01-09 11:17:24 -0500

    pass wordList

--- src/x-snc-firework/LogoAnimation.js
+++ src/x-snc-firework/LogoAnimation.js
@@ -1,34 +1,34 @@
 import CanvasAnimation from './CanvasAnimation';
-import {COLOR} from './Colors';
+import { COLOR } from './Colors';
 
 export default class LogoAnimation extends CanvasAnimation {
-    constructor(mcanvas, start_time, end_time, radius, position, wordList, clearFirst) {
-        super(mcanvas, "LogoAnimation", start_time, end_time)
-        this.radius = radius;
+	constructor(mcanvas, start_time, end_time, radius, position, wordList, clearFirst) {
+		super(mcanvas, "LogoAnimation", start_time, end_time)
+		this.radius = radius;
 		this.pos = position;
 		this.wordList = wordList;
 		this.clearFirst = clearFirst || false;
-    }
-    render(mcanvas) {
+	}
+	render(mcanvas) {
 		var margin = 20;
 		let radius = this.radius;
-		var diam = (2 * radius) ;
+		var diam = (2 * radius);
 		diam += margin;
 
 		if (this.clearFirst) {
-			        // Clear the entire canvas
-		this.context.fillStyle = 'white'; // alpha value provides trailing effect
-		this.context.fillRect(0, 0, mcanvas.getWidth(), mcanvas.getHeight());
+			// Clear the entire canvas
+			this.context.fillStyle = 'white'; // alpha value provides trailing effect
+			this.context.fillRect(0, 0, mcanvas.getWidth(), mcanvas.getHeight());
 		}
 
-		mcanvas.drawCircle( this.pos.x, this.pos.y, radius, 2, COLOR["Dark Green"]);   // CENTER
+		mcanvas.drawCircle(this.pos.x, this.pos.y, radius, 2, COLOR["Dark Green"]);   // CENTER
 
-        mcanvas.drawCircle( this.pos.x, this.pos.y - diam,  radius, 2, "#80B6A1"); // NORTH
+		mcanvas.drawCircle(this.pos.x, this.pos.y - diam, radius, 2, "#80B6A1"); // NORTH
 
-		mcanvas.drawCircle( this.pos.x, this.pos.y + diam, radius, 2, COLOR["Light Green"]);  // SOUTH
-		mcanvas.drawCircle( this.pos.x - diam, this.pos.y, radius, 2, COLOR["Light Green"]); // EAST
-		mcanvas.drawCircle( this.pos.x + diam, this.pos.y, radius, 2, COLOR["Light Green"]);  // WEST
-		
+		mcanvas.drawCircle(this.pos.x, this.pos.y + diam, radius, 2, COLOR["Light Green"]);  // SOUTH
+		mcanvas.drawCircle(this.pos.x - diam, this.pos.y, radius, 2, COLOR["Light Green"]); // EAST
+		mcanvas.drawCircle(this.pos.x + diam, this.pos.y, radius, 2, COLOR["Light Green"]);  // WEST
+
 		if (this.wordList) {
 			mcanvas.drawText(this.pos.x, this.pos.y - diam, this.wordList[0]);
 			mcanvas.drawText(this.pos.x - diam, this.pos.y, this.wordList[1]);
@@ -37,6 +37,7 @@ export default class LogoAnimation extends CanvasAnimation {
 		}
 
 
-		mcanvas.drawText(this.pos.x, this.pos.y + ( 1.7*diam), "Creator");
-		mcanvas.drawText(this.pos.x, this.pos.y + ( 1.7*diam) + 50, "Workflows");    }
+		mcanvas.drawText(this.pos.x, this.pos.y + (1.7 * diam), "Creator");
+		mcanvas.drawText(this.pos.x, this.pos.y + (1.7 * diam) + 50, "Workflows");
+	}
 }
