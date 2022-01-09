b3c0955d5fd0556022a02921038e0b0f62a908ab
MarcMouries <marc@mouries.net>
2022-01-09 11:17:37 -0500

    new

---
+++ src/x-snc-firework/TextAnimation.js
@@ -0,0 +1,29 @@
+import CanvasAnimation from './CanvasAnimation';
+import {COLOR, COLOR_VALUES} from './Colors';
+
+export default class TextAnimation extends CanvasAnimation {
+    constructor(mcanvas, text, start_time, end_time, position, clearFirst, text) {
+        super(mcanvas, "TextAnimation", start_time, end_time)
+        this.text = text;
+		this.pos = position;
+		this.clearFirst = clearFirst || false;
+
+    }
+    render(mcanvas) {
+		var radius = 80;
+		var margin = 20;
+		var diam = (2 * radius) ;
+		diam += margin;
+		
+		if (this.clearFirst) {
+			// Clear the entire canvas
+			this.context.fillStyle = 'white'; // alpha value provides trailing effect
+			this.context.fillRect(0, 0, mcanvas.getWidth(), mcanvas.getHeight());
+		}
+        // Clear the entire canvas
+		//this.context.fillStyle = 'white'; // alpha value provides trailing effect
+		//this.context.fillRect(0, 0, mcanvas.getWidth(), mcanvas.getHeight());
+		
+		this.mcanvas.drawText(this.pos.x, this.pos.y, this.text);
+	}
+}
