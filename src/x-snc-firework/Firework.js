7430df69a10c330faa2c0ffceea026172f58a061
MarcMouries <marc@mouries.net>
2022-01-09 10:41:30 -0500

    with Animation

--- src/x-snc-firework/Firework.js
+++ src/x-snc-firework/Firework.js
@@ -1,13 +1,55 @@
+import Vector from './Vector'
+import Particle from './Particle';
+
 import CanvasAnimation from './CanvasAnimation';
 
 export default class Firework extends CanvasAnimation {
 
-    constructor(duration, mcanvas) {
-        super("Firework", duration, mcanvas)
-        
+    constructor(mcanvas, start_time, end_time) {
+        super(mcanvas, "Firework", start_time, end_time)
+
+        this.gravity = new Vector(0, 0.03);
+        this.friction = 0.99;
+        this.SPEED = 12;
+        this.PARTICLE_COUNT = 400;
+        this.PARTICLE_RADIUS = 3;
+        // spread particle around the circle
+        this.ANGLE_PARTICLE = (Math.PI * 2) / this.PARTICLE_COUNT;
+        this.particles = [];
+
     }
-    render () {
-        
-        console.log("Firework to be implemented. ")
+    render(mcanvas) {
+        // Clear the entire canvas
+		this.context.fillStyle = 'rgba(0,0,0, 0.05)'; // alpha value provides trailing effect
+		this.context.fillRect(0, 0, mcanvas.getWidth(), mcanvas.getHeight());
+
+        // draw particles
+        this.particles.forEach((particle, index) => {
+            // as opacity is continuously decreased when it's below zero it's reset 
+            if (particle.opacity > 0) {
+                particle.draw(this.context)
+                particle.update(this.gravity, this.friction);
+            }
+            else {
+                this.particles.splice(index, 1)
+            }
+        })
+    }
+
+    createParticles(mouse) {
+
+        for (let i = 0; i < this.PARTICLE_COUNT; i++) {
+            let color = `hsl(${Math.random() * 360}, 50%, 50%)`;
+            // spread particles evenly around the circle
+            let angle = (this.ANGLE_PARTICLE * i)
+
+            let velocity = new Vector(
+                Math.cos(this.ANGLE_PARTICLE * i) * (Math.random() * this.SPEED),
+                Math.sin(this.ANGLE_PARTICLE * i) * (Math.random() * this.SPEED)
+            )
+            let position = new Vector(mouse.x, mouse.y);
+            let particle = new Particle(position, this.PARTICLE_RADIUS, color, velocity);
+            this.particles.push(particle)
+        }
     }
 }
--- src/x-snc-firework/LogoAnimation.js
+++ src/x-snc-firework/LogoAnimation.js
@@ -1,11 +1,42 @@
 import CanvasAnimation from './CanvasAnimation';
+import {COLOR} from './Colors';
 
 export default class LogoAnimation extends CanvasAnimation {
-    constructor(duration, mcanvas) {
-        super("LogoAnimation", duration, mcanvas)
-        
+    constructor(mcanvas, start_time, end_time, radius, position, wordList, clearFirst) {
+        super(mcanvas, "LogoAnimation", start_time, end_time)
+        this.radius = radius;
+		this.pos = position;
+		this.wordList = wordList;
+		this.clearFirst = clearFirst || false;
     }
-    render () {
-        console.log("LogoAnimation to be implemented")
-    }
+    render(mcanvas) {
+		var margin = 20;
+		let radius = this.radius;
+		var diam = (2 * radius) ;
+		diam += margin;
+
+		if (this.clearFirst) {
+			        // Clear the entire canvas
+		this.context.fillStyle = 'white'; // alpha value provides trailing effect
+		this.context.fillRect(0, 0, mcanvas.getWidth(), mcanvas.getHeight());
+		}
+
+		mcanvas.drawCircle( this.pos.x, this.pos.y, radius, 2, COLOR["Dark Green"]);   // CENTER
+
+        mcanvas.drawCircle( this.pos.x, this.pos.y - diam,  radius, 2, "#80B6A1"); // NORTH
+
+		mcanvas.drawCircle( this.pos.x, this.pos.y + diam, radius, 2, COLOR["Light Green"]);  // SOUTH
+		mcanvas.drawCircle( this.pos.x - diam, this.pos.y, radius, 2, COLOR["Light Green"]); // EAST
+		mcanvas.drawCircle( this.pos.x + diam, this.pos.y, radius, 2, COLOR["Light Green"]);  // WEST
+		
+		if (this.wordList) {
+			mcanvas.drawText(this.pos.x, this.pos.y - diam, this.wordList[0]);
+			mcanvas.drawText(this.pos.x - diam, this.pos.y, this.wordList[1]);
+			mcanvas.drawText(this.pos.x + diam, this.pos.y, this.wordList[2]);
+			mcanvas.drawText(this.pos.x, this.pos.y + diam, this.wordList[3]);
+		}
+
+
+		mcanvas.drawText(this.pos.x, this.pos.y + ( 1.7*diam), "Creator");
+		mcanvas.drawText(this.pos.x, this.pos.y + ( 1.7*diam) + 50, "Workflows");    }
 }
