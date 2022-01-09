a6078d20e9e853ba6da8e64c05b4fbf27429f60c
MarcMouries <marc@mouries.net>
2022-01-09 10:39:46 -0500

    with Vector

--- src/x-snc-firework/Particle.js
+++ src/x-snc-firework/Particle.js
@@ -1,35 +1,32 @@
-export default class Particle {
-    constructor(x, y, radius, color, velocity) {
-      this.x = x
-      this.y = y
-      this.radius = radius
-      this.color = color
-      this.velocity = {
-        x: velocity.x,
-        y: velocity.y
-      }
-      this.opacity = 1
-    }
-  
-    draw(context) {
-      context.save()
-      context.globalAlpha = this.opacity
-      context.beginPath()
-      context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-      context.fillStyle = this.color
-      context.fill()
-      context.closePath()
-      context.restore()
-    }
-  
-    update(gravity, friction) {
-      this.velocity.x *= friction
-      this.velocity.y *= friction
-      
-      this.velocity.y += gravity
+import Vector from './Vector'
 
-      this.x += this.velocity.x
-      this.y += this.velocity.y
-      this.opacity -= 0.003
-    }
+export default class Particle {
+  constructor(position, radius, color, velocity) {
+    this.pos = position
+    this.radius = radius
+    this.color = color
+    this.velocity = velocity;
+    this.opacity = 1
   }
+
+  draw(context) {
+    context.save()
+    context.globalAlpha = this.opacity
+    context.beginPath()
+    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2, false)
+    context.fillStyle = this.color
+    context.fill()
+    context.closePath()
+    context.restore()
+  }
+
+  update(gravity, friction) {
+    this.velocity.mult(friction);
+
+    this.velocity.add(gravity);
+
+    this.pos.add(this.velocity)
+
+    this.opacity -= 0.003
+  }
+}
